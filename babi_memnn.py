33f7785bc927b1cf01fc712915ecf64b6a2d62fd
seanreed1111 <seanreed1111@gmail.com>
2017-11-29 10:32:18 -0500

    fixes multiple conversion errors in parsestories and getstories

--- babi_memnn.py
+++ babi_memnn.py
@@ -46,7 +46,7 @@ def parse_stories(lines, only_supporting=False):
     data = []
     story = []
     for line in lines:
-        line = line.decode('utf-8').strip()
+        line = line.strip()
         nid, line = line.split(' ', 1)
         nid = int(nid)
         if nid == 1:
@@ -70,7 +70,7 @@ def parse_stories(lines, only_supporting=False):
     return data
 
 
-def get_stories(f, only_supporting=False, max_length=None):
+def get_stories(filename, only_supporting=False, max_length=None):
     '''Given a file name, read the file,
     retrieve the stories,
     and then convert the sentences into a single story.
@@ -78,9 +78,10 @@ def get_stories(f, only_supporting=False, max_length=None):
     If max_length is supplied,
     any stories longer than max_length tokens will be discarded.
     '''
-    data = parse_stories(f.readlines(), only_supporting=only_supporting)
-    flatten = lambda data: reduce(lambda x, y: x + y, data)
-    data = [(flatten(story), q, answer) for story, q, answer in data if not max_length or len(flatten(story)) < max_length]
+    with open(filename) as f:
+        data = parse_stories(f.readlines(), only_supporting=only_supporting)
+        flatten = lambda data: reduce(lambda x, y: x + y, data)
+        data = [(flatten(story), q, answer) for story, q, answer in data if not max_length or len(flatten(story)) < max_length]
     return data
 
 
@@ -120,15 +121,15 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
 # test_stories = get_stories(tar.extractfile(challenge.format('test')))
 
 dirpath = os.path.join('/babi-tasks','babi', 'tasks_1-20_v1-2','en-10k')
-single-fact-path = os.path.join(dirpath,'qa1_single-supporting-fact_{}.txt' )
-two-fact-path = os.path.join(dirpath, 'qa2_two-supporting-facts_{}.txt'
-  )
+single_fact_path = os.path.join(dirpath,'qa1_single-supporting-fact_{}.txt')
+
+two_fact_path = os.path.join(dirpath, 'qa2_two-supporting-facts_{}.txt')
 
 challenges = {
     # QA1 with 10,000 samples
-    'single_supporting_fact_10k': single-fact-path,
+    'single_supporting_fact_10k': single_fact_path,
     # QA2 with 10,000 samples
-    'two_supporting_facts_10k': two-fact-path,
+    'two_supporting_facts_10k': two_fact_path,
 }
 
 
